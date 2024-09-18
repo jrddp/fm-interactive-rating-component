5fbfb3b2a4713be94402a5fc5cef5ecb4a4f8a1d
Jared Peters <jar3dpt@gmail.com>
2024-09-18 14:53:15 -0400

    refactor

--- src/components/RatingCard.tsx
+++ src/components/RatingCard.tsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 import { cn } from "@/lib/utils";
-import { ThankYouIllustration } from "./svgs/ThankYouIllustration";
+import { Star } from "./svgs/Star";
+import { ThankYou } from "./svgs/ThankYou";
 
 export default function RatingCard() {
     const [rating, setRating] = useState<number | null>(null);
@@ -14,7 +15,7 @@ export default function RatingCard() {
         return (
             <Card className="flex flex-col w-full max-w-md gap-4 p-8 border-none rounded-3xl">
                 <div className="flex items-center justify-center mb-6 rounded-full size-14 bg-muted">
-                    <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614" /></svg>
+                    <Star />
                 </div>
 
                 <h1 className="text-3xl">
@@ -42,7 +43,7 @@ export default function RatingCard() {
     } else {
         return (
             <Card className="flex flex-col items-center w-full max-w-md gap-8 p-10 text-center border-none rounded-3xl">
-                <ThankYouIllustration />
+                <ThankYou />
 
                 <div className="px-4 py-2 text-sm rounded-full bg-muted text-primary w-fit">
                     You selected {rating} out of 5
